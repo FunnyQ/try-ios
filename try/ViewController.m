51965998075d353d64b4062a555c4b7eeb7bcfcc
FunnyQ <funnyq@funnyq.com>
2015-10-28 19:48:35 +0800

    [add] add a button and label

--- try/ViewController.m
+++ try/ViewController.m
@@ -10,18 +10,49 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIButton *kaoYao;
+@property (weak, nonatomic) IBOutlet UILabel *labelContainer;
+
+@property NSArray *arrPoet;
+@property int poetStatus;
+
 @end
+
+
+//---------------------------------------------------------------------------
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _arrPoet = @[@"床前明月光", @"疑是地上霜", @"舉頭望明月", @"低頭思故鄉"];
+    _poetStatus = 0;
 }
+
+
+- (IBAction)changeLabelName:(id)sender {
+    NSLog(@"%d", _poetStatus);
+    
+    [self increseStatus];
+    
+    _labelContainer.text = _arrPoet[_poetStatus];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (void) increseStatus {
+    _poetStatus += 1;
+    
+    if (_poetStatus == 4) {
+        _poetStatus = 0;
+    }
 }
 
 @end
